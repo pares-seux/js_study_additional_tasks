b0a5b6522d165fc54c15265341f790ebeb4c3a49
pares-seux <paresseux.992@gmail.com>
2022-01-28 14:56:35 +0400

    lesson-2 commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = 'Урок 2';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 9;
+let rollback = 50;
+let fullPrice = 100;
+let adaptive = false;
 
-alert("кукусики!");
-console.log("урок первый");
+console.log('Типы данных: \n title - ' + typeof(title) + ',\n fullPrice - ' + typeof(fullPrice) + ',\n adaptive - ' + typeof(adaptive) + '.' );
+console.log('Длина строки: ' + screens.length + '.');
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)));
