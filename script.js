f9bf2966b4dd77876c8ac84fdca38c55645c4893
pares-seux <paresseux.992@gmail.com>
2022-02-02 22:52:13 +0400

    lesson 5

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 "use strict";
 
-const stringForFunction = "     tr122334444444444444444444uetruetruetruetrue";
-
-const printString = function (str) {
-  if (typeof str !== "string") {
-    return "Алярм: строка не строка!";
+const arr = ["123", "456", "789", "24680", "13579", "4444", "2222"];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].charAt(0) === "2" || arr[i].charAt(0) === "4") {
+    console.log(arr[i]);
   }
-  str = str.trim();
-  if (str.length > 30) {
-    return str.slice(0, 29) + "...";
-  }
-  return str;
-};
+}
 
-console.log(printString(stringForFunction));
+function searchNumber(num) {
+  for (let i = 2; i < Math.round(num / 2); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function searchLoop(num) {
+  console.log(1 + " Делители этого числа: 1 и 1");
+  console.log(2 + " Делители этого числа: 1 и 2");
+  for (let i = 3; i <= num; i += 2) {
+    if (searchNumber(i)) {
+      console.log(i + " Делители этого числа: 1 и " + i);
+    }
+  }
+}
+
+searchLoop(100);
