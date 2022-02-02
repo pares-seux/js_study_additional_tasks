9e84f6e9950e115b239be3c76cc928487f911f81
pares-seux <paresseux.992@gmail.com>
2022-02-02 16:50:50 +0400

    lesson 4

--- script.js
+++ script.js
@@ -1,62 +1,16 @@
 "use strict";
 
-let lang = "ru";
-let week = new Map([
-  [
-    "ru",
-    [
-      "Понедельник",
-      "Вторник",
-      "Среда",
-      "Четверг",
-      "Пятница",
-      "Суббота",
-      "Воскресенье",
-    ],
-  ],
-  [
-    "en",
-    [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ],
-  ],
-]);
-console.log("#1");
-if (lang === "ru") {
-  console.log(week.get("ru").toString());
-} else if (lang === "en") {
-  console.log(week.get("en") + "");
-} else {
-  console.log("Мы такого языка не знаем");
-}
-console.log("#1");
-switch (lang) {
-  case "ru":
-    console.log(week.get("ru") + "");
-    break;
-  case "en":
-    console.log(week.get("en").toString());
-    break;
-  default:
-    console.log("Мы такого языка не знаем");
-}
-console.log("#3");
-console.log(week.get(lang) + "");
+const stringForFunction = "     tr122334444444444444444444uetruetruetruetrue";
 
-console.log("_______________________________________");
+const printString = function (str) {
+  if (typeof str !== "string") {
+    return "Алярм: строка не строка!";
+  }
+  str = str.trim();
+  if (str.length > 30) {
+    return str.slice(0, 29) + "...";
+  }
+  return str;
+};
 
-let namePerson = "Кирилл";
-
-namePerson === "Артем"
-  ? console.log("Директор " + namePerson)
-  : namePerson === "Александр"
-  ? console.log("преподаватель " + namePerson)
-  : namePerson === "Кирилл"
-  ? console.log("лучший куратор " + namePerson)
-  : console.log("студент " + namePerson);
+console.log(printString(stringForFunction));
