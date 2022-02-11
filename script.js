819bac567a9762c08f51c8d7bfd3040fa428ec87
pares-seux <paresseux.992@gmail.com>
2022-02-11 09:57:32 +0400

    Fixed: made one function

--- script.js
+++ script.js
@@ -20,24 +20,14 @@ function formatDate(num) {
   return num.padStart(2, '0')
 }
 
-function formatTime(num) {
-  if (num === 1 || (num%10 === 1 && num !==11)) {
-    return 'а';
-  }
-  if ((num > 1 && num <5) || (num%10 > 1 && num%10<5 && num > 20)) {
-    return 'ы';
-  }
-   return '';
-}
-
- function formatWords(num) {
-   if (+num === 1 || +num === 21) { return '';}
-   if (+num >=2 && +num < 5) { return 'а';}
-   return 'ов';
+ function formatWords(num, array) {
+   if (num === 1 || (num%10 === 1 && num !==11)) { return array[0];}
+   if ((num > 1 && num <5) || (num%10 > 1 && num%10<5 && num > 20)) { return array[1];}
+   return array[2];
  }
 
 setInterval(function() {
   date = new Date();
-  longString.textContent ='Сегодня ' + week[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + ' час' + formatWords(date.getHours()) +' ' + date.getMinutes() + ' минут' + formatTime(date.getMinutes()) + ' ' + date.getSeconds() + ' секунд' + formatTime(date.getSeconds());
+  longString.textContent ='Сегодня ' + week[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + formatWords(date.getHours(), [' час',' часа',' часов']) +' ' + date.getMinutes() + formatWords(date.getMinutes(), [' минута',' минуты',' минут']) + ' ' + date.getSeconds() + formatWords(date.getSeconds(), [' секунда',' секунды',' секунд']);
   shortString.textContent = formatDate(date.getDate()) + '.' + formatDate(date.getMonth()+1) + '.' + date.getFullYear() + ' - ' + formatDate(date.getHours()) + ':' + formatDate(date.getMinutes()) + ':' +formatDate(date.getSeconds()); 
 }, 1000);
